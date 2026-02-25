652c94998270ff91f83edd6cc1da6878981f2084
Gagan Deep <gagan.deep@isteer.com>
2026-02-25 11:51:24 +0530

    Updated API logic

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API working ✅" });
+});
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
