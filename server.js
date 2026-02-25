24f4634f10b48b7caba0ed0799041e1f077549b7
Gagan Deep <gagan.deep@isteer.com>
2026-02-25 11:27:38 +0530

    Updated API logic

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const { Pool } = require("pg");
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
